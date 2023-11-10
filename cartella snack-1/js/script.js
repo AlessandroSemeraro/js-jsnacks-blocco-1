17bc86826353b468e19f4ff51d50e97840bc771d
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-10 12:08:48 +0100

    Fixed  wrong syntax on second console.log

--- cartella snack-1/js/script.js
+++ cartella snack-1/js/script.js
@@ -3,15 +3,17 @@
 const userMomAge = prompt('how old is your mother?');
 
 
+
 const userDadAge = prompt('how old is your father?');
+
 
 if ( userMomAge > userDadAge ) {
 
-   console.log ('my mom is' + userMomAge + 'old') 
+   console.log ('my mom is ' + userMomAge + ' years old') ;
  }
 
- else if ( userMomAge > userDadAge ) {
+else if ( userMomAge < userDadAge ) {
     
-    console.log ('my dad is' + userDadAge + 'old')
+    console.log('my dad is ' + userDadAge + ' years old');
 
 } 
