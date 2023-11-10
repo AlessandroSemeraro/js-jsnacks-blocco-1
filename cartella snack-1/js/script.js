30215a961051616bd974c74f266f78e78781f487
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-10 11:53:38 +0100

    snack first try

---
+++ cartella snack-1/js/script.js
@@ -0,0 +1,8 @@
+//chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande//
+
+const userFirstAge = prompt('how old are you?');
+
+
+const userSecondAge = prompt('how old are you?');
+
+if ()
