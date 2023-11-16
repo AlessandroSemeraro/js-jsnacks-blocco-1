703e9e4b45d4ce50227dc8688935a118908b5dbb
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-16 12:32:49 +0100

    added prompt + lenght difference

---
+++ cartella snack-5/js/script.js
@@ -0,0 +1,13 @@
+const userFirstWord = prompt('insert first word');
+console.log(userFirstWord);
+
+const userSecondWord = prompt('insert second word');
+console.log(userSecondWord);
+
+if( userFirstWord.length > userSecondWord.length){
+    console.log('userFirstWord');
+    console.log('userSecondWord');
+}else if ( userFirstWord.length < userSecondWord.length){
+    console.log('userFirstWord');
+    console.log('userSecondWord');
+}
