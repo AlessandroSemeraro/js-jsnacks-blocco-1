a41d179e5d2fa38546c79aeefb2a76133b7bd748
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-16 17:22:11 +0100

    Inizializzazione

---
+++ cartella snack-10/js/script.js
@@ -0,0 +1,7 @@
+const numberX=document.getElementById("random-number").innerHTML = Math.floor(Math.random() * 1000);
+
+const button = document.getElementById('button');
+
+button.addEventListener("click", function() {
+    numberX + 3;
+  }); 
