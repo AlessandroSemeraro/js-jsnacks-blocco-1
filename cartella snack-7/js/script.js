1042bbf620e36780957e96d75beded3e3a796fec
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-22 00:29:00 +0100

    fixed condition

--- cartella snack-7/js/script.js
+++ cartella snack-7/js/script.js
@@ -1,13 +1,12 @@
 const array =[];
 
-for (let i=1; i<=6; i++){
+for (let i=0; i<=6; i++){
     const number=parseInt(prompt('insert  number'));
     
-    if ( number % 2 == 0 ){
+    if ( number % 2 !== 0 ){
         console.log(number);
-    }else if ( number % 2 == 1){
-        const array = [number];
-        console.log(array);
+        array.push(number)
     }
 }
 
+console.log(array)
