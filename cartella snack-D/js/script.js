08e931b5c3e3dd5fe4acf368a210a780c6efb102
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-22 09:29:01 +0100

    added addEventListener

---
+++ cartella snack-D/js/script.js
@@ -0,0 +1,10 @@
+const numberX = document.getElementById("random-number").innerHTML = Math.floor(Math.random() * 1000);
+
+const button = document.getElementById('button');
+console.log(button)
+
+const sum = 0
+
+button.addEventListener("click", function() {
+ 
+  }); 
